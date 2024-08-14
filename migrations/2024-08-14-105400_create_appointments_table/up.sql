297b55aad9f686dcb1f517d8989953fa660703bd
RhoNit <tb.edumax29@gmail.com>
2024-08-14 18:29:00 +0530

    new migration: added a column in appointments table

--- migrations/2024-08-14-105400_create_appointments_table/up.sql
+++ migrations/2024-08-14-105400_create_appointments_table/up.sql
@@ -2,5 +2,6 @@ CREATE TABLE appointments(
     id SERIAL PRIMARY KEY,
     patient_id INTEGER REFERENCES patients(id),
     doctor_id INTEGER REFERENCES doctors(id),
-    appointment_status VARCHAR NOT NULL
+    time_slot VARCHAR NOT NULL,
+    status VARCHAR NOT NULL
 );
